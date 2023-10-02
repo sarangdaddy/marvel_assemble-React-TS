d15afc6f3148b656a5349c36564ebbdbf6c31b8b
sarang_daddy <sarang_daddy@naver.com>
2023-10-02 23:55:10 +0900

    Fix : Link 경로 수정
    - 절대경로 추가

--- src/components/Character/index.tsx
+++ src/components/Character/index.tsx
@@ -2,13 +2,14 @@ import { Link } from 'react-router-dom';
 
 import * as S from './styles';
 import { CharacterTypes } from '@/types';
+import { ROUTE_PATH } from '@/router/routePath';
 
 export const Character = ({ id, name, thumbnail }: CharacterTypes) => {
   return (
     <S.Character_container>
       <S.Movie_img src={`${thumbnail.path}.${thumbnail.extension}`} alt="img" />
       <S.Movie_name>
-        <Link to={`/character/${id}`}>{name}</Link>
+        <Link to={ROUTE_PATH.DETAIL.replace(':id', id.toString())}>{name}</Link>
       </S.Movie_name>
     </S.Character_container>
   );
